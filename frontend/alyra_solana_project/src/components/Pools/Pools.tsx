1ec41728834a12422767063cf4b069220360195a
Neal-hackathons <dr2ecsce@duck.com>
2024-07-14 13:58:52 +0200

    fix(PoolsScreen): fixed build + minor fixes

--- frontend/alyra_solana_project/src/components/Pools/Pools.tsx
+++ frontend/alyra_solana_project/src/components/Pools/Pools.tsx
@@ -3,34 +3,54 @@
 import { columns } from "./columns";
 import { DataTable } from "./data-table";
 
-import { createContext, useState, useEffect, useContext, useMemo } from "react";
+import {  useState, useEffect, useMemo } from "react";
 import { useAnchorWallet, useConnection } from "@solana/wallet-adapter-react";
-import { Pool } from "./types";
+import type { Pool } from "./types";
 import { getProgram } from "../../lib/utils";
-import {Wallet} from "@project-serum/anchor";
-
+import type { Wallet } from "@project-serum/anchor";
 
 export function PoolsTable() {
-	
+	const [pools, setPools] = useState<Pool[]>([]);
+
 	const { connection } = useConnection();
 	const wallet = useAnchorWallet();
 	const program = useMemo(() => {
-    if (connection) { return getProgram(connection, wallet as Wallet); }
-  	}, [connection, wallet]);
-	const pools = program.account.pool.all();
+		if (connection) {
+			return getProgram(connection, wallet as Wallet);
+		}
+	}, [connection, wallet]);
 
-	let pools_vect:Pool[] =[];
-	const poolElement:Pool = {
-		pool: /*pool.name*/ "Sol" ,
-		description: /*pool.descritpion*/ "Desc",
-		yield: /*pool.poolYield*/ 5.9,
-	}
-	pools_vect.fill(poolElement);
+	useEffect(() => {
+		if (!program) return;
+		(async () => {
+			try {
+				const allPools = await program?.account.pool.all();
+				console.log(allPools);
+				const pools = allPools?.map((pool) => {
+					console.log(`Pool ${pool}`);
+
+					/**
+					 * TODO:Write mapping code
+					 * to map the data returned by program?.account.pool.all()
+					 * to a Pool type : { pool: string; description: string; yield: number; }
+					 */
+					return {
+						pool: pool.publicKey.toBase58(),
+						description: /*pool.descritpion*/ "Desc",
+						yield: /*pool.poolYield*/ 5.9,
+					};
+				});
+				setPools(pools || []);
+			} catch (error) {
+				console.log("SOMETHING WENT WRONG");
+				console.error(error);
+			}
+		})();
+	}, [program]);
 
 	//const pools_vect = { Pool : Array(pools.length)};
 	//const pools_vect = new Pool[pools.length];
 	//pools.forEach((pool,index) => {
-		//console.log('Pool ${pool}');
 	//	const poolElement:Pool = {
 	//		pool: /*pool.name*/ "Sol" ,
 	//		description: /*pool.descritpion*/ "Desc",
@@ -41,7 +61,7 @@ export function PoolsTable() {
 
 	return (
 		<div className="container mx-auto py-10">
-			<DataTable columns={columns} data={pools_vect} />
+			<DataTable columns={columns} data={pools} />
 		</div>
 	);
 }
--- frontend/alyra_solana_project/src/screens/Homescreen/hometable/hometable.tsx
+++ frontend/alyra_solana_project/src/screens/Homescreen/hometable/hometable.tsx
@@ -1,15 +1,8 @@
-"use client";
-
 import { columns } from "./columns";
 import { DataTable } from "./data-table";
 import { SAMPLE_DATA } from "./sample-data";
 
-
-
-
 export function HomeTable() {
-	
-
 	return (
 		<div className="container mx-auto py-10">
 			<DataTable columns={columns} data={SAMPLE_DATA} />
