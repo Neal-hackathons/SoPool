717bc162f40fba8eb4075d177196629680427f7e
Neal-hackathons <dr2ecsce@duck.com>
2024-07-15 21:33:20 +0200

    feat(CI): added Vercel env variables

--- frontend/alyra_solana_project/src/lib/constants.js
+++ frontend/alyra_solana_project/src/lib/constants.js
@@ -1,5 +1,11 @@
 import { PublicKey } from "@solana/web3.js";
 
+
+
 export const PROGRAM_ID = new PublicKey(
-  "3attdVJrdoB1J82iRdp3Gpo8KmKUK4R4SB5fzKRLFNpH" 
+  process.env.NEXT_PUBLIC_POOL_PROGRAM_ID ?? "3attdVJrdoB1J82iRdp3Gpo8KmKUK4R4SB5fzKRLFNpH"
+);
+
+export const STAKING_PROGRAM_ID = new PublicKey(
+  process.env.NEXT_PUBLIC_STAKING_PROGRAM_ID ?? "DQ2xiRbRvJuymAZSTj7kd7T8Auagh49NuKv6jS8kpX5c"
 );
