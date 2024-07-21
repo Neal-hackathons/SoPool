1fb1f8a88547d9b59c2632a20ab5219a514de168
Neal-hackathons <dr2ecsce@duck.com>
2024-07-21 10:30:24 +0200

    fix: attempt #1 at fixing SOL number parsing

--- frontend/alyra_solana_project/src/components/Admin/admin.tsx
+++ frontend/alyra_solana_project/src/components/Admin/admin.tsx
@@ -2,20 +2,25 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import type { Connection } from '@solana/web3.js';
+import type { Connection } from "@solana/web3.js";
 import { BN } from "bn.js";
-import {  useMemo} from "react";
-import { type AnchorWallet, useAnchorWallet, useConnection } from "@solana/wallet-adapter-react";
+import { useMemo } from "react";
+import {
+	type AnchorWallet,
+	useAnchorWallet,
+	useConnection,
+} from "@solana/wallet-adapter-react";
 import { useState } from "react";
-import { getNewLotteryAddress, getLotteryProgram, getMasterAddress } from "../../lib/utils";
+import {
+	getNewLotteryAddress,
+	getLotteryProgram,
+	getMasterAddress,
+} from "../../lib/utils";
 import type { Wallet, Program } from "@coral-xyz/anchor";
 import type { Lottery } from "../../types/lottery";
 
-
-
 function init_master(program: Program<Lottery>, wallet: AnchorWallet) {
-		
-    if (!program || !wallet) return;
+	if (!program || !wallet) return;
 	try {
 		const txHash = program.methods
 			.initMaster()
@@ -43,12 +48,12 @@ export function InitMaster() {
 		return (
 			<div className="container mx-auto py-10">
 				<Button className="max-w-xs mx-auto" disabled>
-                    No program or wallet found
+					No program or wallet found
 				</Button>
 			</div>
 		);
 	}
-    
+
 	return (
 		<div className="container mx-auto py-10">
 			<Button
@@ -63,34 +68,38 @@ export function InitMaster() {
 	);
 }
 
-const create_loss_lottery = async (program: Program<Lottery>, wallet: AnchorWallet, connection: Connection, price: number) => {
-    
-    if (price <= 0) return;
-    const priceLamport = new BN(price * 1000000000);
+const create_loss_lottery = async (
+	program: Program<Lottery>,
+	wallet: AnchorWallet,
+	connection: Connection,
+	price: number,
+) => {
+	if (price <= 0) return;
+	const priceLamport = new BN(price * 1000000000);
 
-    if (!program || !wallet) return;
+	if (!program || !wallet) return;
 	try {
-        const masterAddress = await getMasterAddress();
-        console.log("master ", masterAddress.toString());
+		const masterAddress = await getMasterAddress();
+		console.log("master ", masterAddress.toString());
 
-        const accountData = await program.account.master.fetch(masterAddress);
-        if (!accountData) {
-            throw new Error('Compte non trouvé');
-        }
+		const accountData = await program.account.master.fetch(masterAddress);
+		if (!accountData) {
+			throw new Error("Compte non trouvé");
+		}
 
-        //console.log("masterInfo ", accountData.lastId);
-       
-        const lotteryAddress = await getNewLotteryAddress(accountData.lastId);
-        //console.log("lottery ", lotteryAddress.toString());
-        //console.log("lamport ", priceLamport);
+		//console.log("masterInfo ", accountData.lastId);
 
-        const txHash = await program.methods
+		const lotteryAddress = await getNewLotteryAddress(accountData.lastId);
+		//console.log("lottery ", lotteryAddress.toString());
+		//console.log("lamport ", priceLamport);
+
+		const txHash = await program.methods
 			.createLottery(priceLamport)
 			.accounts({
-                lottery: lotteryAddress,
+				lottery: lotteryAddress,
 				// @ts-expect-error
-                master: masterAddress,
-	            payer: wallet.publicKey,
+				master: masterAddress,
+				payer: wallet.publicKey,
 			})
 			//.signers([wallet.publicKey])
 			.rpc();
@@ -100,13 +109,11 @@ const create_loss_lottery = async (program: Program<Lottery>, wallet: AnchorWall
 		console.log("SOMETHING WENT WRONG in create lottery");
 		console.error(error);
 	}
-
-
-}
+};
 export function CreateLossLottery() {
-	const [price, setPrice] = useState('');
-    
-    const { connection } = useConnection();
+	const [price, setPrice] = useState("");
+
+	const { connection } = useConnection();
 	const wallet = useAnchorWallet();
 	const program = useMemo(() => {
 		if (connection && wallet) {
@@ -118,29 +125,35 @@ export function CreateLossLottery() {
 		return (
 			<div className="container mx-auto py-10">
 				<Button className="max-w-xs mx-auto" disabled>
-                    No program or wallet found
+					No program or wallet found
 				</Button>
 			</div>
 		);
 	}
-    
+
 	return (
 		<div className="container mx-auto py-10">
-			<Input type="number"
-                    id="price"
-                    value={price}
-                    placeholder="Ticket Price (SOL)"
-                    onChange={(e) => setPrice(e.target.value)}
-            />
-            <Button
+			<Input
+				type="number"
+				id="price"
+				value={price}
+				placeholder="Ticket Price (SOL)"
+				onChange={(e) => setPrice(e.target.value)}
+			/>
+			<Button
 				className="max-w-xs mx-auto"
 				onClick={async () => {
-					create_loss_lottery(program, wallet, connection ,Number.parseInt(price));
+					create_loss_lottery(
+						program,
+						wallet,
+						connection,
+						Number.parseFloat(price),
+					);
 				}}
 			>
 				Create Lottery
 			</Button>
-            <text> (funds are transferred to the winner)</text>
+			<text> (funds are transferred to the winner)</text>
 		</div>
 	);
 }
